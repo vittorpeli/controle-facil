8c6691a15953ec82cbc01c89c1e52c1282e1245f
vittor <vittor.pelicarto137@gmail.com>
2026-04-25 18:27:18 -0300

    feature: add table footer to table component

--- app/routes/styles-guide.table.tsx
+++ app/routes/styles-guide.table.tsx
@@ -1,37 +1,46 @@
 import { ShoppingBag } from "lucide-react";
 import { ComponentPresentation } from "~/features/styles-guide/ComponentPresentation";
-import { Table, TableBody, TableCell, TableCellIcon, TableHead, TableHeader, TableRow } from "~/ui/table";
+import { Table, TableBody, TableCell, TableCellIcon, TableFooter, TableHead, TableHeader, TableRow } from "~/ui/table";
 
 const exampleTable = () => (
-    <Table>
-        <TableHeader>
-            <TableRow>
-                <TableHead>Date</TableHead>
-                <TableHead>Merchant</TableHead>
-                <TableHead>Category</TableHead>
-                <TableHead>Amount</TableHead>
-            </TableRow>
-        </TableHeader>
+    <div className="table__wrapper">
+        <Table>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Merchant</TableHead>
+                    <TableHead>Category</TableHead>
+                    <TableHead>Amount</TableHead>
+                </TableRow>
+            </TableHeader>
 
-        <TableBody>
-            <TableRow>
-                <TableCell>Oct 24</TableCell>
-                <TableCell>Starbucks</TableCell>
-                <TableCell>Food &amp; Dining</TableCell>
-                <TableCell>-$5.40</TableCell>
-            </TableRow>
+            <TableBody>
+                <TableRow>
+                    <TableCell>Oct 24</TableCell>
+                    <TableCell>Starbucks</TableCell>
+                    <TableCell>Food &amp; Dining</TableCell>
+                    <TableCell>-$5.40</TableCell>
+                </TableRow>
 
-            <TableRow>
-                <TableCell>Oct 23</TableCell>
-                <TableCell>
-                    <TableCellIcon><ShoppingBag /></TableCellIcon>
-                    Whole Foods
-                </TableCell>
-                <TableCell>Groceries</TableCell>
-                <TableCell>-$142.30</TableCell>
-            </TableRow>
-        </TableBody>
-    </Table>
+                <TableRow>
+                    <TableCell>Oct 23</TableCell>
+                    <TableCell>
+                        <TableCellIcon><ShoppingBag /></TableCellIcon>
+                        Whole Foods
+                    </TableCell>
+                    <TableCell>Groceries</TableCell>
+                    <TableCell>-$142.30</TableCell>
+                </TableRow>
+            </TableBody>
+
+            <TableFooter>
+                <TableRow>
+                    <TableCell colSpan={3}>Total</TableCell>
+                    <TableCell className="text-right font-mono text-step--1">-$142.30</TableCell>
+                </TableRow>
+            </TableFooter>
+        </Table>
+    </div>
 )
 
 export default function StylesGuideTable () {
